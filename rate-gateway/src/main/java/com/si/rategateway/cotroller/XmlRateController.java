d5d23b90bf1ffbb5fbf4f61d443c52c095eb580e
Rosen <rosen.neshevski@openpayd.com>
2022-11-27 21:53:15 +0200

    Added ResponseEntity to the XmlRateController

--- rate-gateway/src/main/java/com/si/rategateway/cotroller/XmlRateController.java
+++ rate-gateway/src/main/java/com/si/rategateway/cotroller/XmlRateController.java
@@ -8,6 +8,7 @@ import com.si.rategateway.infrastructure.mapper.RateMapper;
 import com.si.rategateway.service.RateService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,13 +25,14 @@ public class XmlRateController {
     private final RateService rateService;
 
     @PostMapping(path = "command", consumes = MediaType.APPLICATION_XML_VALUE, produces = MediaType.APPLICATION_XML_VALUE)
-    public XmlCommandResponseList requestCurrentCurrencyInfo(@RequestBody XmlCommandRequest request) {
+    public ResponseEntity<XmlCommandResponseList> requestCurrentCurrencyInfo(@RequestBody XmlCommandRequest request) {
         XmlCommandDto xmlCommandDto = RateMapper.mapXmlCommandDto(request);
         XmlCommandResponseList xmlCommandResponseList = new XmlCommandResponseList();
         List<XmlCommandResponse> responses = rateService.commandCurrency(xmlCommandDto).stream()
                 .map(RateMapper::mapToXmlCommandResponse)
                 .collect(Collectors.toList());
         xmlCommandResponseList.setXmlCommandResponse(responses);
-        return xmlCommandResponseList;
+        return ResponseEntity.ok()
+                .body(xmlCommandResponseList);
     }
 }
